bfacf6e8568924211e6fa271eac80d9aa52d6582
Harishwar Reddy Pulla <hpulla@Harishwars-MacBook-Pro.local>
2017-06-28 07:36:41 +0530

    new repo

---
+++ src/main/java/com/training/hadoop/App.java
@@ -0,0 +1,14 @@
+package com.training.hadoop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String name = "harish12345";
+        System.out.println(name.substring(6,9));
+    }
+}
